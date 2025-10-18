c065f2adaf61d3cd1bc8b24f2dddb91b4035353c
JesdaGH <jsarato@miumg.edu.gt>
2025-10-18 16:23:23 -0600

    Segundo commit

---
+++ roundcube/config/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+// Config overrides for branding
+$config['product_name'] = 'MyTeamRedesI';
+$config['language'] = 'es_ES';
+$config['skin'] = 'elastic';
+$config['support_url'] = 'https://myteamredesi.local/soporte';
+// Plugins (habilita/deshabilita según necesites)
+$config['plugins'] = ['archive','zipdownload'];
